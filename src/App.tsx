34572730b1d55cd3868d2f0f57426f9c4daa56ef
MIKAzhccnn <zcliu1922@gmail.com>
2024-02-08 21:20:22 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,9 @@ import "./App.css";
 
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Zhicheng Liu said, Hello World!
-            </p>
+        <div>
+            <h1>UD CISC275 Tasks</h1>
+            <p>Zhicheng Liu said, Hello World!</p>
         </div>
     );
 }
