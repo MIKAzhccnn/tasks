5dd08141708d327a02b15cdf2ec6f8e8638b739b
MIKAzhccnn <zcliu1922@gmail.com>
2024-02-13 14:55:06 -0500

    Updates the web

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
+import GTR_R32 from "./images/GTR_R32.jpg";
 
 function App(): JSX.Element {
     return (
-        <div>
-            <h1>UD CISC275 Tasks</h1>
+        <div className="App">
+            <header className="App-header">UD CISC275 Tasks</header>
+            <h1>GTR R32 showcase</h1>
             <p>Zhicheng Liu said, Hello World!</p>
+            <img src={GTR_R32} alt="GTR!!!" />
+            <br></br>
+            R32 is:
+            <ul>
+                <li>FAST</li>
+                <li>STRONG</li>
+                <li>BEAUTY</li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
         </div>
     );
 }
